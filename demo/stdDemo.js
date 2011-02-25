7df56e61990614b16ec4b66b7702a36fe4e13844
Stella Laurenzo <stella@laurenzo.org>
2011-02-25 14:11:47 -0800

    Import code from other project

---
+++ demo/stdDemo.js
@@ -0,0 +1,28 @@
+var http=require('http');
+var webSocketHandler=require('../lib/webSocketHandler');
+
+function handleSocket(s) {
+	s.on('message', function(body) {
+		console.log('Got message: "' + body.toString('utf8') + '"');
+		s.sendTextMessage(body);
+	});
+	
+	s.connect();
+}
+
+var server=http.createServer(function(req, res) {
+	var ws=res.webSocket;
+	if (ws) {
+		handleSocket(ws);
+	} else {
+		// Punt - we don't do anything else here
+		res.writeHead(500);
+		res.end('We only speak WebSockets here');
+	}
+});
+
+webSocketHandler.configureServer(server);
+
+server.listen(4080);
+console.log('Server running');
+
